60d7e2a401498d55b6ce5b566ad4fdb87c6366a7
VAMSI AILA <avamsi.web@gmail.com>
2018-08-15 15:50:00 +0530

    dependencies initialization compleated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
+import { AboutModule } from './about/about.module';
+import { ContactModule } from './contact/contact.module';
+import { GalleryModule } from './gallery/gallery.module';
+import { ServicesModule } from './services/services.module';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    HomeModule,
+    AboutModule,
+    ContactModule,
+    GalleryModule,
+    ServicesModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MainNavbarComponent, FooterComponent]
+})
+export class SharedModule { }
